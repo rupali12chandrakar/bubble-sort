23d2d0e0c27313353b5753d09b92ac7944a1b5b8
Rupali Chandrakar <148358159+rupali12chandrakar@users.noreply.github.com>
2024-07-11 14:17:48 +0530

    Update insertionsort.c

--- insertionsort.c
+++ insertionsort.c
@@ -1,1 +1,29 @@
+//insertion sort c program
 
+#include<stdio.h>
+int main(){
+  
+  int arr[]={ 9,3, 45,8,1,4};
+  int size=6;
+
+  for(int i=1;i<size-1;i++){
+       int temp=arr[i];
+       int j=i-1;
+    for(;j>=0;j--){
+  
+if (temp<arr[j]){
+ arr[j+1]=arr[j];
+}
+else{
+break;}
+}
+    
+arr[j+1]=temp;}
+printf("sorted array: ");
+  
+for (int k=0;k<size;k++){
+  
+printf("%d ",arr[k]);
+}
+return 0;
+}
